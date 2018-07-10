b409551b8ef70e0ac9bd3759aec293ee9aeba0b6
Matti Aho <ookooko@gmail.com>
2018-07-10 20:13:42 +0300

    cors added. ready for deployment to net

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
+app.use(cors());
 
 // middleware
 const logger = (request, response, next) => {
