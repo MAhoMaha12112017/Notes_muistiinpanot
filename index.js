87fa414580803a2c51658b8cbc5945afce913002
Matti Aho <ookooko@gmail.com>
2018-07-10 20:39:47 +0300

    Herokumuutoksia

--- index.js
+++ index.js
@@ -93,5 +93,7 @@ const error = (request, response) => {
 app.use(error);
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
